ff816859fb3cc6742fbe60cf8dfa99f23da0fbd0
yugalkhanal <yugalkhanal5@gmail.com>
2025-02-22 21:09:52 +0000

    test fix to unavailable file download

--- p2p/tracker.go
+++ p2p/tracker.go
@@ -286,8 +286,8 @@ type CleanupConfig struct {
 // DefaultCleanupConfig returns the default cleanup configuration
 func DefaultCleanupConfig() CleanupConfig {
 	return CleanupConfig{
-		InactivityThreshold: 30 * time.Second,  // Reduced from 2 minutes
-		CleanupInterval:     2 * time.Second, // Reduced from 30 seconds
+		InactivityThreshold: 120 * time.Second,  // Reduced from 2 minutes
+		CleanupInterval:     15 * time.Second, // Reduced from 30 seconds
 	}
 }
 
